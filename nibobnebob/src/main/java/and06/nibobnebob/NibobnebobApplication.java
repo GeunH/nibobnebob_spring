848762b6033a5ea722f5ff7d8ce9de0aa4b93f08
최근혁 <unlxrmsgur@naver.com>
2024-02-24 10:11:40 +0900

    Add files via upload

---
+++ nibobnebob/src/main/java/and06/nibobnebob/NibobnebobApplication.java
@@ -0,0 +1,13 @@
+package and06.nibobnebob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NibobnebobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NibobnebobApplication.class, args);
+	}
+
+}
---
+++ nibobnebob/src/test/java/and06/nibobnebob/NibobnebobApplicationTests.java
@@ -0,0 +1,13 @@
+package and06.nibobnebob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NibobnebobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
